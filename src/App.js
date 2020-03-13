b33bb360ce18a97c37563a742d7ddb183d2f0d9a
yinshengabc <756039435@qq.com>
2020-03-13 02:21:48 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Button } from "reactstrap";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [res, setRes] = useState();
+
+  const getresult = a => {
+    a.preventDefault();
+    const file = a.target.files[0];
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "audio/wave");
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: file,
+      redirect: "follow"
+    };
+    fetch("http://localhost:1337/localhost:80/stt", requestOptions)
+      .then(response => response.text())
+      .then(result => setRes(result))
+      .catch(error => console.log("error", error));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div class="container">
+        <h2>
+          This is a page for the client part of ASR task (based on DeepSpeech)
+        </h2>
+        <div class="row">
+          <div class="offset-md-3 col-md-6"></div>
+          <div class="form-group files">
+            <label>Upload Your wav File </label>
+            <input type="file" name="file" onChange={getresult} />
+          </div>
+        </div>
+        <p>Detection result:</p>
+        {res !== null ? <p>{res}</p> : <p>no result</p>}
+        <Button onClick={() => setRes(null)}>Reset</Button>
+      </div>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
